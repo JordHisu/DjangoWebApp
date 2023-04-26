36d37e14a84336f0e86d592016e4a511ed6874a1
JordVCS <jord_aqui@hotmail.com>
2023-04-26 14:42:26 -0300

    changes to settings

--- app/WebAppProject/settings.py
+++ app/WebAppProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import socket
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,9 +27,28 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8020', 'http://127.0.0.1:8020', 'http://0.0.0.0:8020']
-CORS_ALLOWED_ORIGINS = ['http://localhost:8020', 'http://127.0.0.1:8020']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8020', 'http://127.0.0.1:8020', 'http://0.0.0.0:8020', socket.gethostbyname(socket.gethostname())]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 # Application definition
 
